789862f3884e733c45d58caf70619f032452ef9b
GaelFG17 <gaelfg1720@gmail.com>
2024-09-03 20:41:46 -0600

    Inicio

---
+++ Pygame/juego.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+
+# Inicializar Pygame
+pygame.init()
+
+# Configuración de la pantalla
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Juego de Disparos 2D")
