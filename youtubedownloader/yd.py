f8311fb17e56160302411f37e2d50e51f7bb7252
GaelFG17 <gaelfg1720@gmail.com>
2024-09-12 20:49:13 -0600

    Fin de la logoca para la manipulacion y descarga

--- youtubedownloader/yd.py
+++ youtubedownloader/yd.py
@@ -4,5 +4,12 @@ def download_video(url, path):
     try:
         # Crear un objeto de YouTube
         yt = YouTube(url)
+        
+        # Obtener el stream de mayor resolución
+        stream = yt.streams.get_highest_resolution()
+
+        # Descargar el video
+        print(f"Descargando {yt.title}...")
+        stream.download(output_path=path)
     except Exception as e:
         print(f"Error al descargar el video: {e}")
