f40ce9528f6ea3b6813b960637441cf0b56c7f0a
GaelFG17 <gaelfg1720@gmail.com>
2024-09-12 20:52:46 -0600

    FIN

--- youtubedownloader/yd.py
+++ youtubedownloader/yd.py
@@ -1,4 +1,9 @@
 from pytube import YouTube
+import ssl
+import certifi
+
+# Configurar Python para usar los certificados de certifi
+ssl._create_default_https_context = ssl.create_default_context(cafile=certifi.where())
 
 def download_video(url, path):
     try:
@@ -13,3 +18,9 @@ def download_video(url, path):
         stream.download(output_path=path)
     except Exception as e:
         print(f"Error al descargar el video: {e}")
+        
+if __name__ == "__main__":
+    video_url = input("Introduce la URL del video de YouTube: ")
+    save_path = input("Introduce la ruta donde se guardará el video: ")
+    
+    download_video(video_url, save_path)
