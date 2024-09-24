d139e310defdb66c578a68cb0383f03bf9e85cd0
GaelFG17 <gaelfg1720@gmail.com>
2024-09-23 20:59:20 -0600

    Funcion de heuristica

--- gps/tspa.py
+++ gps/tspa.py
@@ -7,3 +7,7 @@ def distancia(ciudad1, ciudad2):
     lat1, lon1 = coord[ciudad1]
     lat2, lon2 = coord[ciudad2]
     return math.sqrt((lat2 - lat1)**2 + (lon2 - lon1)**2)
+
+
+def heuristica(ciudad_actual, destino):
+    return distancia(ciudad_actual, destino)
