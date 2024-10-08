c789d2622bde926282e2ed363d41f8ba3320e9de
GaelFG17 <gaelfg1720@gmail.com>
2024-10-07 19:13:50 -0600

    Importaciones y configuracion de arduino

---
+++ detectorplacas/back.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request, jsonify
+import Recog as Re
+import conector as cn
+import serial
+import time
+
+app = Flask(__name__)
+
+arduino = serial.Serial('/dev/cu.usbserial-130', 9600, timeout=1)
