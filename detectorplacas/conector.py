acec39773652cf9528fb4403e919761a4cf6fea2
GaelFG17 <gaelfg1720@gmail.com>
2024-10-08 19:17:57 -0600

    Creacion de la funcion pata la conexion, con un bloque try para intentar la conexion y en caso de error devolver un mensaje

--- detectorplacas/conector.py
+++ detectorplacas/conector.py
@@ -1,1 +1,14 @@
 import mysql.connector
+
+def conectar_db():
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="placass"
+        )
+        return mydb
+    except mysql.connector.Error as err:
+        print(f"Error de conexión a la base de datos: {err}")
+        return None
